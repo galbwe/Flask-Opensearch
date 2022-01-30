a6efb19ce6c0fb791c58e61893d240f16b66e35e
Wes Galbraith <galbwe92@gmail.com>
2022-01-29 18:07:45 -0700

    Add help text to wait for opensearch script

--- scripts/wait_for_opensearch.py
+++ scripts/wait_for_opensearch.py
@@ -6,13 +6,13 @@ from opensearchpy import OpenSearch
 
 
 @click.command()
-@click.option("-h", "--host", default="localhost", type=str)
-@click.option("-p", "--port", default=9200, type=int)
-@click.option("-u", "--user", default="admin", type=str)
-@click.option("-w", "--password", default="", type=str)
-@click.option("-n", "--number-tries", "tries", default=5, type=int)
-@click.option("-s", "--seconds", default=5, type=int)
-@click.option("-y", "--skip-prompts", "skip_prompts", is_flag=True)
+@click.option("-h", "--host", default="localhost", type=str, help="The host name of the openesearch cluster. Can be set with OPENSEARCH_HOST environment variable.")
+@click.option("-p", "--port", default=9200, type=int, help="The port opensearch accepts connections on. Can be set with OPENSEARCH_PORT environment variable.")
+@click.option("-u", "--user", default="admin", type=str, help="The opensearch user to authenticate with. Can be set with OPENSEARCH_USER environment variable.")
+@click.option("-w", "--password", default="", type=str, help="The opensearch password to authenticate with. Can be set with OPENSEARCH_PASSWORD environment variable.")
+@click.option("-n", "--number-tries", "tries", default=5, type=int, help="The maximum number of times to retry connecting to the opensearch cluster.")
+@click.option("-s", "--seconds", default=5, type=int, help="The time in seconds between connection attempts.")
+@click.option("-y", "--skip-prompts", "skip_prompts", is_flag=True, help="Skip password prompts.")
 def wait_for_opensearch(host, port, user, password, tries, seconds, skip_prompts):
 
     if not password and not skip_prompts:
