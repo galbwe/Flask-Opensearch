a601a73e26917e64666b7a05e810c64d4576e39c
Wes Galbraith <galbwe92@gmail.com>
2022-02-09 03:38:30 -0700

    Actually remove non-deterministic assertions this time

--- tests/test_readme_examples.py
+++ tests/test_readme_examples.py
@@ -56,24 +56,6 @@ def test_readme_example():
         res_3 = opensearch.search(
             body={"size": 5, "query": {"multi_match": {"query": "miller", "fields": ["title^2", "director"]}}}
         )
-        assert res_3 == {
-            "took": 156,
-            "timed_out": False,
-            "_shards": {"total": 3, "successful": 3, "skipped": 0, "failed": 0},
-            "hits": {
-                "total": {"value": 1, "relation": "eq"},
-                "max_score": 0.2876821,
-                "hits": [
-                    {
-                        "_index": "test-index",
-                        "_type": "_doc",
-                        "_id": "1",
-                        "_score": 0.2876821,
-                        "_source": {"title": "Moneyball", "director": "Bennett Miller", "year": "2011"},
-                    }
-                ],
-            },
-        }
         assert res_3["timed_out"] is False
         assert res_3["hits"]["total"]["value"] == 1
         assert res_3["hits"]["total"]["relation"] == "eq"
@@ -91,16 +73,6 @@ def test_readme_example():
             index="test-index",
             id="1",
         )
-        assert res_4 == {
-            "_index": "test-index",
-            "_type": "_doc",
-            "_id": "1",
-            "_version": 2,
-            "result": "deleted",
-            "_shards": {"total": 2, "successful": 1, "failed": 0},
-            "_seq_no": 1,
-            "_primary_term": 1,
-        }
         assert res_4["_index"] == "test-index"
         assert res_4["_type"] == "_doc"
         assert res_4["_id"] == "1"
